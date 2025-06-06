0e9d7e74121954af8d3fc2acab66e9ffb0bfa841
Henrique <henrixe80@gmail.com>
2025-06-06 14:46:23 -0400

    Adicionando logica de temas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ThemeService } from './services/theme.service';
+import { ServiceAppService } from './service-app.service';
 
 /**
  * @ignore
@@ -10,4 +12,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  constructor(
+    private themeService: ThemeService,
+    private ltiService: ServiceAppService
+  ) {}
+  ngOnInit(): void {
+    this.ltiService.getDadosCompletos();
+    this.themeService.limparTemaSalvo();
+
+    const tema = this.ltiService.dados_completos.plataforma.temaTipo
+    const cores = {
+      primaria: `${this.ltiService.dados_completos.plataforma.customPrimaria}`,
+      secundaria: `${this.ltiService.dados_completos.plataforma.customSecundaria}`,
+      terciaria: `${this.ltiService.dados_completos.plataforma.customTerciaria}`,
+      quartenaria: `${this.ltiService.dados_completos.plataforma.customQuartenaria}`,
+      quintenaria: `${this.ltiService.dados_completos.plataforma.customQuintenaria}`,
+    }
+
+    this.themeService.aplicarTema(tema, cores);
+  }
 }
