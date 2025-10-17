cf86dbae32c62bae5773f20b4c2c2d5932b14697
KaueWallace <kauewall8@gmail.com>
2025-10-17 17:20:13 -0400

    adicionando metadata com informação do módulo no chat n8n

--- src/app/components/chat-n8n/chat-n8n.component.ts
+++ src/app/components/chat-n8n/chat-n8n.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { ServiceAppService } from 'src/app/service-app.service';
 
 @Component({
   selector: 'app-chat-n8n',
@@ -7,21 +8,32 @@ import { Component, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 })
 export class ChatN8nComponent implements OnInit {
   private chatScript!: HTMLScriptElement;
+  private nomeModulo!: string;
 
-  constructor(private renderer: Renderer2) {}
+  constructor(private renderer: Renderer2, private apiService: ServiceAppService) {}
 
   ngOnInit(): void {
+    this.apiService.getDadosCompletos()
+    
+    if (this.apiService.dados_completos.length !== 0) {
+      this.nomeModulo = this.apiService.dados_completos.modulo.nome_modulo
+    }
+
+    // https://tecnocomp.uea.edu.br:5678/webhook/8e3b74b3-5941-4a21-a583-c07d8c45c1cb/chat/${this.nomeModulo}
+
     const link = this.renderer.createElement('link');
     link.rel = 'stylesheet';
     link.href = 'https://cdn.jsdelivr.net/npm/@n8n/chat/dist/style.css';
     this.renderer.appendChild(document.head, link);
-
     this.chatScript = this.renderer.createElement('script');
     this.chatScript.type = 'module';
     this.chatScript.text = `
       import { createChat } from 'https://cdn.jsdelivr.net/npm/@n8n/chat/dist/chat.bundle.es.js';
       createChat({
-        webhookUrl: 'https://tecnocomp.uea.edu.br:5678/webhook/8e3b74b3-5941-4a21-a583-c07d8c45c1cb/chat',
+        webhookUrl: 'https://tecnocomp.uea.edu.br:5678/webhook/c3f42851-79f7-4579-ad10-a24e53ddb10b/chat',
+        metadata: {
+          modulo: '${this.nomeModulo}'
+        },
 	initialMessages: [
 		'Seja bem vindo! 👋',
 		'Inicie uma conversa com nosso assistente.'
